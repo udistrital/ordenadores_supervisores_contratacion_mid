8c6e821dca459d1320817231c8af3e6c4d6060ce
EliSLopezM <emprendimientoeli@gmail.com>
2024-11-15 09:17:29 -0500

    feat: integracion de controladores al codigo udistrital/argo_documentacion/issues/195

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // Accedemos al ConfigService para obtener las configuraciones desde el .env
+  const configService = app.get(ConfigService);
+
+  // Obtenemos el puerto desde el archivo .env o usamos 3000 por defecto
+  const port = configService.get<number>('PORT') ?? 3000;
+
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
